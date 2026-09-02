8842b969b547f58106f6a48b328e49740eeb7dd7
Josh <josh@c4raven.net>
2026-09-02 21:36:27 +1000

    Update auto-generated version stamp

--- raven/__init__.py
+++ raven/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "1.7.10"
-__version_tuple__ = (1, 7, 10)
+__version__ = "1.7.11-post.20+61088c0"
+__version_tuple__ = (1, 7, 11, "post", 20, "61088c0")
